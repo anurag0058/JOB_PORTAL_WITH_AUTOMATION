3008f68539c47d4222665f2151d31b2b3e4f802a
ANURAG <rathourasr0508@gmail.com>
2025-04-19 01:13:14 +0530

    getuser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./App.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
@@ -12,9 +12,14 @@ import PostApplication from "./pages/PostApplication"
 import Register from "./pages/Register";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useDispatch } from 'react-redux'
+import { getUser } from './store/slices/userSlice'
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getUser());
+  },[])
   return (
     <>
        <Router>
